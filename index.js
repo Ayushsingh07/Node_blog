bb844760cc353f8bd477d0c4d302bcbdcd28e062
Ayushsingh07 <nqu7069@gmail.com>
2022-09-29 22:26:19 +0530

    new comt

---
+++ index.js
@@ -0,0 +1,40 @@
+const express= require('express')
+const dotenv=require("dotenv")
+const mongoose=require("mongoose")
+const authRoute=require("./routes/auth")
+const app=express()
+
+dotenv.config()
+
+app.use(express.json())
+
+
+mongoose.connect(process.env.mongodb).then(console.log("connected to DB")).catch((err)=>console.log(err));
+
+app.use("/auth",authRoute)
+
+// app.use("/",(req,res) =>{
+//     console.log("url ")
+// })
+
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    console.log(error);
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error
+      }
+    });
+  });
+
+app.listen('5000',()=>{
+    console.log("running good gg")
+})  
+
